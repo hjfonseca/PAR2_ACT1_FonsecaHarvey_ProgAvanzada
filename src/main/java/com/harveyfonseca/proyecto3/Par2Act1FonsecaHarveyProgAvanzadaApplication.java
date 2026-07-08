9e2efa8c81c3a1a88886d0e1c7381e411b20c9a4
Harvy Fns <hjfonseca@espe.edu.ec>
2026-07-08 15:47:14 -0500

    chore: inicializa proyecto spring webflux

---
+++ src/main/java/com/harveyfonseca/proyecto3/Par2Act1FonsecaHarveyProgAvanzadaApplication.java
@@ -0,0 +1,13 @@
+package com.harveyfonseca.proyecto3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Par2Act1FonsecaHarveyProgAvanzadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Par2Act1FonsecaHarveyProgAvanzadaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/harveyfonseca/proyecto3/Par2Act1FonsecaHarveyProgAvanzadaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harveyfonseca.proyecto3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Par2Act1FonsecaHarveyProgAvanzadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
